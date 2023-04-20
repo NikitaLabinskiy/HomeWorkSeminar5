be1695af3c0b77b0fb5fc99077bde4620ecb339f
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-04-20 18:03:18 +0300

    Вне изменения в класс Database

--- src/main/java/Model/Database.java
+++ src/main/java/Model/Database.java
@@ -1,8 +1,10 @@
 package Model;
 
 
+import java.util.Scanner;
+
 public interface Database {
     void add(Employee employee);
     void remove(Integer id);
-    void changeData(Integer inputMenu, Integer employeeId, String newData);
+    void changeData(Integer inputMenu, Integer employeeId, String inputData);
 }
