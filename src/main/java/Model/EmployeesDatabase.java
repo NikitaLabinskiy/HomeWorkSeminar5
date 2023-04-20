d7e966db9cc6d5c2ae11a9f6fb42049282250237
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-04-20 18:05:57 +0300

    Вне изменения в класс EmployeesDatabase

--- src/main/java/Model/EmployeesDatabase.java
+++ src/main/java/Model/EmployeesDatabase.java
@@ -1,11 +1,15 @@
 package Model;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
+@Data
 public class EmployeesDatabase implements Database{
-    private final List<Employee> database;
+    private List<Employee> database;
 
     public EmployeesDatabase() {
         this.database = new ArrayList<>();
@@ -21,45 +25,49 @@ public class EmployeesDatabase implements Database{
         Iterator<Employee> iterator = database.iterator();
         while (iterator.hasNext()){
             Employee employee = iterator.next();
-            if(employee.getUniqueID() == id){
+            if(employee.getUniqueId() == id){
                 iterator.remove();
                 break;
             }
         }
     }
 
+    public List<Employee> getDatabase() {
+        return database;
+    }
+
     @Override
-    public void changeData(Integer inputMenu, Integer employeeId, String newData) {
+    public void changeData(Integer inputMenu, Integer employeeId, String inputData) {
+        String newData = null;
         if(inputMenu == 1){
             for (Employee employee : database) {
-                if(employee.getUniqueID() == employeeId)
-                    employee.setFirstName(newData);
+                if(employee.getUniqueId() == employeeId)
+                    employee.setFirstName(inputData);
             }
         }
         if(inputMenu == 2){
             for (Employee employee : database) {
-                if(employee.getUniqueID() == employeeId)
-                    employee.setSecondName(newData);
+                if(employee.getUniqueId() == employeeId)
+                    employee.setSecondName(inputData);
             }
         }
         if(inputMenu == 3){
             for (Employee employee : database) {
-                if(employee.getUniqueID() == employeeId)
-                    employee.setAge(Integer.parseInt(newData));
+                if(employee.getUniqueId() == employeeId)
+                    employee.setAge(Integer.parseInt(inputData));
             }
         }
         if(inputMenu == 4){
             for (Employee employee : database) {
-                if(employee.getUniqueID() == employeeId)
-                    employee.setSpeciality(newData);
+                if(employee.getUniqueId() == employeeId)
+                    employee.setSpeciality(inputData);
             }
         }
         if(inputMenu == 5){
             for (Employee employee : database) {
-                if(employee.getUniqueID() == employeeId)
+                if(employee.getUniqueId() == employeeId)
                     employee.setSalary(Double.parseDouble(newData));
             }
         }
-
     }
 }
