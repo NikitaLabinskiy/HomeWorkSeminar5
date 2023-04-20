f00ddb232e61ed0dfe390dcf56a87b569749ede6
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-04-20 18:03:52 +0300

    Вне изменения в класс Employee

--- src/main/java/Model/Employee.java
+++ src/main/java/Model/Employee.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 @Data
 public class Employee {
-    private static Integer id = 0;
-    Integer uniqueId;
+    private static int id = 0;
+    int uniqueId;
     String firstName;
     String secondName;
     Integer age;
@@ -13,14 +13,21 @@ public class Employee {
     Double salary;
 
     public Employee(String firstName, String secondName, Integer age, String speciality, Double salary) {
-        this.uniqueId = getUniqueID();
+        this.uniqueId = getEmployeeID();
+//        this.uniqueId = id++;
         this.firstName = firstName;
         this.secondName = secondName;
         this.age = age;
         this.speciality = speciality;
         this.salary = salary;
     }
-    protected int getUniqueID(){
-        return this.uniqueId = ++id;
+    protected int getEmployeeID(){
+        return ++id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee: id - "+ uniqueId + ", " + firstName + ", " + secondName +
+                ", " + age + " лет, " + speciality + ", salary - " + salary;
     }
 }
